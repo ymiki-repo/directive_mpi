7b79dd7119e395d96d5dd2aa00eaf30ef467a070
Yohei MIKI <ymiki@cc.u-tokyo.ac.jp>
2025-10-18 13:55:34 +0900

    edit C/openacc/1_diffusion/0_original

--- C/openacc/1_diffusion/0_original/main.c
+++ C/openacc/1_diffusion/0_original/main.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
-    
+
     int nprocs = 1;
     int rank   = 0;
 
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
     const int rank_up   = rank != nprocs - 1 ? rank + 1 : MPI_PROC_NULL;
     const int rank_down = rank != 0          ? rank - 1 : MPI_PROC_NULL;
-    
+
     const int nx0 = 128;
     const int ny0 = nx0;
     const int nz0 = nx0;
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
     const int nx = nx0;
     const int ny = ny0;
     const int nz = nz0 / nprocs;
-    
+
     if (nz * nprocs != nz0) {
         if (rank == 0) {
             fprintf(stdout, "Error: nz(%d) * nprocs(%d) != nz0(%d)\n", nz, nprocs, nz0);
@@ -41,46 +41,48 @@ int main(int argc, char *argv[])
 
     for (int r=0; r<nprocs; r++) {
         MPI_Barrier(MPI_COMM_WORLD);
-        if (r != rank) continue;
+        if (r != rank){
+            continue;
+        }
 
         char hostname[128];
         gethostname(hostname, sizeof(hostname));
         fprintf(stdout, "Rank %d: hostname = %s\n", rank, hostname);
         fflush(stdout);
     }
-    
+
     const int mgn = 1;
     const int lnx = nx;
     const int lny = ny;
     const int lnz = nz + 2*mgn;
     const int ln  = lnx*lny*lnz;
-    
-    const float lx = 1.0;
-    const float ly = 1.0;
-    const float lz = 1.0;
-    
+
+    const float lx = 1.0F;
+    const float ly = 1.0F;
+    const float lz = 1.0F;
+
     const float dx = lx/(float)nx0;
     const float dy = ly/(float)ny0;
     const float dz = lz/(float)nz0;
 
-    const float kappa = 0.1;
-    const float dt    = 0.1*fmin(fmin(dx*dx, dy*dy), dz*dz)/kappa;
+    const float kappa = 0.1F;
+    const float dt    = 0.1F*fminf(fminf(dx*dx, dy*dy), dz*dz)/kappa;
 
     const int   nt = 100000;
     double time = 0.0;
     int    icnt = 0;
     double flop = 0.0;
     double elapsed_time = 0.0;
-    
+
     float *f  = (float *)malloc(sizeof(float)*ln);
     float *fn = (float *)malloc(sizeof(float)*ln);
 
     init(nprocs, rank, nx, ny, nz, mgn, dx, dy, dz, f);
-    
+
     MPI_Barrier(MPI_COMM_WORLD);
-    
+
     start_timer();
-    
+
     for (; icnt<nt && time + 0.5*dt < 0.1; icnt++) {
         if (rank == 0 && icnt % 100 == 0) fprintf(stdout, "time(%4d) = %7.5f\n", icnt, time);
 
@@ -91,16 +93,16 @@ int main(int argc, char *argv[])
 
         MPI_Send(&f[nx*ny*mgn]     , nx*ny, MPI_FLOAT, rank_down, tag, MPI_COMM_WORLD);
         MPI_Recv(&f[nx*ny*(nz+mgn)], nx*ny, MPI_FLOAT, rank_up  , tag, MPI_COMM_WORLD, &status);
-        
+
         flop += diffusion3d(nprocs, rank, nx, ny, nz, mgn, dx, dy, dz, dt, kappa, f, fn);
-        
+
         swap(&f, &fn);
 
         time += dt;
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
-    
+
     elapsed_time = get_elapsed_time();
 
     if (rank == 0) {
@@ -109,23 +111,23 @@ int main(int argc, char *argv[])
         fprintf(stdout, "Performance= %7.2f [GFlops/nprocs]\n", performance);
         fprintf(stdout, "             %7.2f [GFlops]\n", performance*nprocs);
     }
-    
+
     const double ferr = err(nprocs, rank, time, nx, ny, nz, mgn, dx, dy, dz, kappa, f);
 
     double ferr_sum = 0.0;
     MPI_Reduce(&ferr, &ferr_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    
+
     const double faccuracy = sqrt(ferr_sum/(double)(nx0*ny0*nz0));
 
     if (rank == 0) {
         fprintf(stdout, "Error[%d][%d][%d] = %10.6e\n", nx0, ny0, nz0, faccuracy);
     }
-    
+
     free(f);  f  = NULL;
     free(fn); fn = NULL;
 
     MPI_Finalize();
-    
+
     return 0;
 }
 
