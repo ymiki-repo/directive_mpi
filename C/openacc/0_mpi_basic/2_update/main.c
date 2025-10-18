3514be8d45d04e660a2b844ec1287cc341dba507
Yohei MIKI <ymiki@cc.u-tokyo.ac.jp>
2025-10-18 13:31:25 +0900

    edit C/openacc/2_update

--- C/openacc/0_mpi_basic/2_update/main.c
+++ C/openacc/0_mpi_basic/2_update/main.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <mpi.h>
-#include <openacc.h>
-
 
 double get_elapsed_time(const struct timeval *tv0, const struct timeval *tv1);
 
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
-    
+
     int nprocs = 1;
     int rank   = 0;
 
@@ -22,12 +20,6 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    const int ngpus = acc_get_num_devices(acc_device_nvidia);
-    if (rank == 0) {
-        fprintf(stdout, "num of GPUs = %d\n", ngpus);
-    }
-    acc_set_device_num(rank % ngpus, acc_device_nvidia);
-    
     const unsigned int nx = 4096;
     const unsigned int ny = 4096;
     const unsigned int n  = nx * ny;
@@ -35,9 +27,9 @@ int main(int argc, char *argv[])
     float *b = malloc(n*sizeof(float));
 
     const unsigned int w  = 10;
-    
+
     MPI_Barrier(MPI_COMM_WORLD);
-    
+
     struct timeval tv0;
     gettimeofday(&tv0, NULL);
 
@@ -46,7 +38,7 @@ int main(int argc, char *argv[])
     double sum = 0.0;
 #pragma acc data create(a[0:n], b[0:n])
     {
-    
+
 #pragma acc kernels copyout(a[0:n], b[0:n])
 #pragma acc loop independent
         for (unsigned int i=0; i<n; i++) {
@@ -75,7 +67,7 @@ int main(int argc, char *argv[])
     /**** End ****/
 
     MPI_Barrier(MPI_COMM_WORLD);
-    
+
     struct timeval tv1;
     gettimeofday(&tv1, NULL);
 
@@ -83,7 +75,7 @@ int main(int argc, char *argv[])
         fprintf(stdout, "mean = %5.2f\n", sum / n);
         fprintf(stdout, "Time = %8.3f [sec]\n", get_elapsed_time(&tv0, &tv1));
     }
-    
+
     free(a);
     free(b);
 
