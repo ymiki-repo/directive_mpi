4a83c680fdd77703bbed8f656c2592ca589c9f68
Yohei MIKI <ymiki@cc.u-tokyo.ac.jp>
2025-10-18 13:41:53 +0900

    add w/ or w/o GPUDirect RDMA

---
+++ sh/miyabi-g/no_gdr_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -q lecture-g
+#PBS -l select=2:mpiprocs=1
+#PBS -l walltime=00:10:00
+#PBS -W group_list=gt00
+
+module purge
+module load nvidia nv-hpcx
+
+cd ${PBS_O_WORKDIR}
+
+export UCX_MEMTYPE_CACHE=n
+export UCX_IB_GPU_DIRECT_RDMA=n
+mpiexec sh/common/wrapper.sh ./run
